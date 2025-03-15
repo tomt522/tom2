8a44553635c69eeb80c90140a63ef6bc0465a3d2
tomt522 <rifatjamil613@gmail.com>
2025-03-15 10:38:50 +0600

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -38,16 +38,13 @@ module.exports = {
 
       // Stylish message design
       const message = `
-┏━━━━━━━━━━━━━━━┓
-   🎭 𝘛ₒₓᵢ𝚌ᵢ𝚝ₑᵣ ⭕
-┗━━━━━━━━━━━━━━━┛
+         𝐓𝐎𝐌🎀
 📆 Uptime: ${uptimeString}
 🙋 Total Users: ${allUsers.length}
 💬 Total Threads: ${allThreads.length}
 🔥 Active Threads: ${activeThreads}
 📨 Total Messages: ${totalMessages}
-━━━━━━━━━━━━━━━━━━━
-💡|H_A_S_A_N_S__B_O_T|
+
       `;
 
       api.sendMessage(message.trim(), event.threadID);
