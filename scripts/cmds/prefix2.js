ae27d12499ef6023dceafa8bccddb3cc298be3c7
tomt522 <rifatjamil613@gmail.com>
2025-03-15 10:37:41 +0600

    Update prefix2.js

--- scripts/cmds/prefix2.js
+++ scripts/cmds/prefix2.js
@@ -15,12 +15,12 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
  body: `
-Yo, my prefix is [ 𓆩 / 𓆪 ]\n
+Yo, my prefix is [ 𓆩 - 𓆪 ]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
-➥ 𝙊𝙒𝙉𝙀𝙍: ~𝙃𝘼𝙎𝘼𝙉
+➥ 𝙊𝙒𝙉𝙀𝙍: ~𝐑𝐢𝐟𝐚𝐭 𝐉𝐚𝐦𝐢𝐥
 ➥ ✓sim [bby] -> talk to bot
 ➥ ✓callad [message] -> report any problem encountered
-➥ ✓help -> usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/Itz.HaSaN.00`,
+➥ ✓help -> usage of command\n\nHave fun using it enjoy!❤️`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/M4luPbE.gif")
  });
  }
